958d0e5d57debe9d751f78549ff1d0df9fae8058
Michael Prouty <michael.prouty@gmail.com>
2013-09-26 12:44:36 -0400

    Make a basic User model (including secure passwords)

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ source 'https://rubygems.org'
 
 gem 'rails', '4.0.0'
 gem 'bootstrap-sass'
+gem 'bcrypt-ruby', '~> 3.0.0'
 gem 'pg'
 
 group :development, :test do
---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ActiveRecord::Base
+  before_save { self.email = email.downcase } 
+  validates_presence_of :name, :email
+  validates :name, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, format: { with: VALID_EMAIL_REGEX }, uniqueness: { 
+    case_sensitive: false }
+  has_secure_password
+  validates :password, length: { minimum: 6 }
+end
