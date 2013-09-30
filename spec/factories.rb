4b1c488ad3b3a5a94340038eb72573387db2f662
Michael Prouty <michael.prouty@gmail.com>
2013-09-30 12:16:28 -0400

    Finish user signup

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name      "Michael Prouty"
+    email     "mprouty@example.com"
+    password  "foobar"
+    password_confirmation "foobar"
+  end
+end
